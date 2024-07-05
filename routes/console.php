ad4bf80de8a3787e7c1c689f50dc35ade806faed
bravo18br <bravo18br@gmail.com>
2024-07-05 13:16:59 -0300

    05/07

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('compare:sites')
-    ->timezone('America/Sao_Paulo')
-    ->between('08:00', '18:00')
-    ->weekdays()
-    ->hourly();
+    ->everyFiveMinutes();
