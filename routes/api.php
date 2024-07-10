6913d5a639b00a06cf71fae0e8ab14d7cb6ef9ce
bravo18br <bravo18br@gmail.com>
2024-07-10 15:37:18 -0300

    10/07

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\WppController;
 use App\Http\Controllers\WppProxyController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/wpp/{any}', [WppProxyController::class, 'proxy'])->where('any', '.*');
+// Route::any('/wpp/{any}', [WppProxyController::class, 'proxy'])->where('any', '.*');
 
 // Route::get('/wpp/api-docs', [WppController::class, 'api_docs']);
 // Route::get('/wpp/gerar-token', [WppController::class, 'gerar_token']);
