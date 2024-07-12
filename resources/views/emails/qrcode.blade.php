7ae631e01a1666a485949a649f427fa844171526
bravo18br <bravo18br@gmail.com>
2024-07-12 16:25:28 -0300

    12/07

--- resources/views/emails/qrcode.blade.php
+++ resources/views/emails/qrcode.blade.php
@@ -14,23 +14,12 @@
 </html>
 
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Code - Monitora Sites</title>
-</head>
-
+<!-- BODY DO GMAIL, APRESENTA DEFEITOS -->
 <body>
-    <p><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQgAAAEICAYAAACj9mr/AAAAAXNSR0IArs4c6QAAHbVJREFUeF7t3VtyZMeRBNDWFjj7oIzc/xpkpoXMGjigmT5miMToNNw766Ll/duR8fDw8IxbqMfffvn1tz++PeDff//zH5TFf//CYQj9T8xnGk8NaXVpAAAAABJRU5ErkJggg==" alt="QR Code"></p>
+    <p><img src=3D"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQgAAAEICA=YAAACj9mr/AAAAAXNSR0IArs4c6QAAHjtJREFUeF7t3VFyLbmRA1B5C5592GHvfw0d4YXMGnqe/=zwtdsRpAc2qJ0PfSSYSiQRZpaurv(SUPRIMIDO)/6WemqQOeTLQiakpvqRerU2HVzHrkKthn/D9=H2kwwBd1pmIDAAAAAElFTkSuQmCC" alt=3D"QR Code"></p>
 </body>
 
+<!-- BODY DO RESEND, APRESENTA O QRCODE PERFEITAMENTE -->
 <body>
-    <p><img src=3D"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQgAAAEICA=
-YAAACj9mr/AAAAAXNSR0IArs4c6QAAHbVJREFUeF7t3VtyZMeRBNDWFjj7oIzc/xpkpoXMGjigm=
-T5miMToNNw766Ll/duR8fDw8IxbqMfffvn1tz++PeDff//zH5TFf//CYQj9T8xnGk8NaXVpAAAAABJRU5ErkJggg=
-=3D=3D" alt=3D"QR Code"></p>
+    <p><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQgAAAEICAYAAACj9mr/AAAAAXNSR0IArs4c6QAAHjtJREFUeF7t3VFyLbmRA1B5C5592GHvfw0d4YXMGnqe/zwtdsRpAc2qJ0PfSSYSiQRZpaurv(SUPRIMIDO)/6WemqQOeTLQiakpvqRerU2HVzHrkKthn/D9H2kwwBd1pmIDAAAAAElFTkSuQmCC" alt="QR Code"></p>
 </body>
-
