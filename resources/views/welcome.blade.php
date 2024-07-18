65177c1ffa524e9dbb9614eae12402c3041416a0
bravo18br <bravo18br@gmail.com>
2024-07-18 16:29:13 -0300

    18/07

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monitora Sites API</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
 </head>
 
 <body>
     <h1>Monitora Sites API</h1>
     <p><a href="https://github.com/bravo18br/api_laravel_web_scrapping">https://github.com/bravo18br/api_laravel_web_scrapping</a></p>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+    <script>
+        const ui = SwaggerUIBundle({
+            url: '/docs/swagger.yaml',
+            dom_id: '#swagger-ui'
+        });
+    </script>
 </body>
 
 </html>
