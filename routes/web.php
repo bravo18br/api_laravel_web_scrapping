9977767162adb2ddcd022174e92fde67190d425e
bravo18br <bravo18br@gmail.com>
2024-07-19 10:32:04 -0300

    19/07

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test-log', function () {
-    Log::channel('integrado')->info('Testando log no canal integrado');
-    return 'Log testado';
 });
