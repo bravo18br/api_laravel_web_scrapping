c6e3846f638210925430b44d6e6bb1aff8a98955
bravo18br <bravo18br@gmail.com>
2024-08-05 13:35:16 -0300

    05/08

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->view('welcome', [], 200);
 })->middleware(HandleCors::class);
-
-// Route::get('/', function () {
-//     return response()->view('welcome', [], 200);
-// });
